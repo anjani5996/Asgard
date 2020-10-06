2a3800cad38b67e6f04618cbd74577686ed8c315
anjani prakash <anjani.prakash@airfi.aero>
2020-10-06 17:30:22 +0530

    check 3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-let num1 = 5;
-let num2 = 3;
+const num1 = 5;
+const num2 = 3;
 
 // add two numbers
-let sum = num1 + num2;
+const sum = num1 + num2;
 
 // display the sum
-console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+console.log(sum);
